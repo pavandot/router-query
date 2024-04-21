fc8ee105e284e619d0aed1e0698505b8a99ba104
pavan <pavandotnet007@outlook.com>
2024-04-21 18:25:23 +0530

    design update

--- src/routes/user/User.jsx
+++ src/routes/user/User.jsx
@@ -7,10 +7,12 @@ export const User = () => {
 	const { data: user } = useQuery(userQuery(params?.userID));
 	return (
 		<div className=' w-full h-full text-gray-100 flex justify-center items-center pt-10'>
-			<div>
+			<div className=' p-2 max-w-md  w-full'>
 				<p className=' text-2xl  font-bold'>{user?.name}</p>
-				<div>
-					<Link to={`/user/${user?.id}/posts`}>Posts</Link>
+				<div className=' mt-2 '>
+					<Link to={`/user/${user?.id}/posts`} className=' underline'>
+						Posts
+					</Link>
 				</div>
 			</div>
 		</div>
